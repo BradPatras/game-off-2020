b939e85bcee97146e03ddb19513a928045f5b404
Brad <BradPatras@users.noreply.github.com>
2020-11-04 22:55:27 -0600

    generated libGDX project

---
+++ android/src/io/github/bradpatras/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.github.bradpatras;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.github.bradpatras.TranslunarGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TranslunarGame(), config);
+	}
+}
